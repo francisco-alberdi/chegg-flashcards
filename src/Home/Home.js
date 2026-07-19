2f7694c30741397a4da6bc22c29f30b04057162f
Francisco Alberdi <francisco_alberdi@bellsouth.net>
2026-07-18 22:13:31 -0700

    Basic API fetching of decks

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CreateDeckButton from "./CreateDeckButton";
+import { listDecks } from "../utils/api/index";
 
 function Home() {
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function loadDecks() {
+      try {
+        const decks = await listDecks(abortController.signal);
+        setDecks(decks);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.error(error);
+        }
+      }
+    }
+    loadDecks();
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
   return (
     <div>
       <CreateDeckButton />
+      <div>
+        {decks.map((deck) => (
+          <div key={deck.id}>{deck.name}</div>
+        ))}
+      </div>
     </div>
   );
 }

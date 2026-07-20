09d262959605e8bda69c9a0730ad80161d871045
Francisco Alberdi <francisco_alberdi@bellsouth.net>
2026-07-19 17:34:56 -0700

    Modifying the nav bar to show the list of links

--- src/Study/index.js
+++ src/Study/index.js
@@ -1,10 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { readDeck } from "../utils/api/index";
 import NavBar from "../components/NavBar";
 
 function Study() {
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState(null);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function loadDeck() {
+      try {
+        const response = await readDeck(deckId, abortController.signal);
+        setDeck(response);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.error(error);
+        }
+      }
+    }
+    loadDeck();
+
+    return () => abortController.abort();
+  }, [deckId]);
+
+  if (!deck) {
+    return <p>Loading...</p>;
+  }
+
+  const navBarItems = [
+    { name: deck.name, url: `/decks/${deck.id}` },
+    { name: "Study" },
+  ];
+
   return (
     <div>
-      <NavBar />
+      <NavBar items={navBarItems} />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function NavBar() {
-  return <Link to="/">Home</Link>;
+function NavBar({ items = [] }) {
+  return (
+    <ol>
+      <Link to="/">Home</Link>
+      {items.map((item, index) => (
+        <li key={index}>
+          <Link to={item.url}>{item.name}</Link>
+        </li>
+      ))}
+    </ol>
+  );
 }
 
 export default NavBar;

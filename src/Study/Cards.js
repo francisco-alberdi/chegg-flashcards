bef853911945a81f21d95455a844a595213f8707
Francisco Alberdi <francisco_alberdi@bellsouth.net>
2026-07-19 18:17:30 -0700

    Handling flipping

--- src/Study/Cards.js
+++ src/Study/Cards.js
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 
 function Cards({ cards = [] }) {
+  const [cardIndex, setCardIndex] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const currentCard = cards[cardIndex];
+
+  const handleFlip = () => {
+    setIsFlipped((prevFlipped) => !prevFlipped);
+  };
+
   return (
     <div>
       <div>
-        <h5>Card #</h5>
-        <p>{cards[0].front}</p>
-        <button className="btn btn-secondary mr-2">Flip</button>
-        <button className="btn btn-primary">Next</button>
+        <h5>Card {cardIndex + 1}</h5>
+        <p>{isFlipped ? currentCard.back : currentCard.front}</p>
+        <button className="btn btn-secondary mr-2" onClick={handleFlip}>
+          Flip
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() =>
+            setCardIndex((prevIndex) => (prevIndex + 1) % cards.length)
+          }
+        >
+          Next
+        </button>
       </div>
     </div>
   );

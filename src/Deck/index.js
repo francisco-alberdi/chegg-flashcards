3a221a1ac9f4badda1a49c2d28f4cd626e871e33
Francisco Alberdi <francisco_alberdi@bellsouth.net>
2026-07-20 21:48:10 -0700

    Adding styling to the buttons on the deck page

--- src/Deck/index.js
+++ src/Deck/index.js
@@ -36,14 +36,33 @@ function Deck() {
     <div>
       <NavBar items={navBarItems} />
 
-      <div>
+      <div className="mb-4">
         <h2>{deck.name}</h2>
         <p>{deck.description}</p>
+        <div className="d-flex justify-content-between">
+          <div className="d-flex">
+            <Link
+              to={`/decks/${deck.id}/edit`}
+              className="btn btn-secondary mr-2"
+            >
+              Edit
+            </Link>
+            <Link
+              to={`/decks/${deck.id}/study`}
+              className="btn btn-primary mr-2"
+            >
+              Study
+            </Link>
+            <Link
+              to={`/decks/${deck.id}/cards/new`}
+              className="btn btn-primary"
+            >
+              + Add Cards
+            </Link>
+          </div>
 
-        <Link to={`/decks/${deck.id}/edit`}>Edit</Link>
-        <Link to={`/decks/${deck.id}/study`}>Study</Link>
-        <Link to={`/decks/${deck.id}/cards/new`}>Add Cards</Link>
-        <button>Delete</button>
+          <button className="btn btn-danger">Delete</button>
+        </div>
       </div>
 
       <h3>Cards</h3>
